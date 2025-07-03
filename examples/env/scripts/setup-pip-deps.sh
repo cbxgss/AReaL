3bf0864d6752e9e2d6c1734591610bbcc4aeba49
chengrong <chengrong@tju.edu.cn>
2025-07-03 15:08:10 +0800

    w

--- examples/env/scripts/setup-pip-deps.sh
+++ examples/env/scripts/setup-pip-deps.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # basic dependencies
-pip install -U pip
-pip uninstall torch deepspeed flash-attn pynvml cugraph-dgl dask-cuda cugraph-service-server raft-dask cugraph cuml cugraph-pyg -y
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0
-pip install "sglang[all]==0.4.6.post4" 
-pip install megatron-core==0.11.0 nvidia-ml-py
-pip install git+https://github.com/garrett4wade/cugae --no-build-isolation --verbose
-pip install "flash-attn<=2.7.3" --no-build-isolation
+uv pip install -U pip
+uv pip uninstall torch deepspeed flash-attn pynvml cugraph-dgl dask-cuda cugraph-service-server raft-dask cugraph cuml cugraph-pyg -y
+uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0
+uv pip install "sglang[all]==0.4.6.post4" 
+uv pip install megatron-core==0.11.0 nvidia-ml-py
+uv pip install git+https://github.com/garrett4wade/cugae --no-build-isolation --verbose
+uv pip install "flash-attn<=2.7.3" --no-build-isolation
 
 # Package used for calculating math reward
-pip install -e evaluation/latex2sympy
+uv pip install -e evaluation/latex2sympy
 
 # Install an editable sglang
 rm -rf ./sglang
@@ -17,8 +17,8 @@ git clone -b v0.4.6.post4 https://github.com/sgl-project/sglang
 AREAL_PATH=$PWD
 cd sglang
 git apply ../patch/sglang/v0.4.6.post4.patch
-pip install -e "python[all]" --no-deps
+uv pip install -e "python[all]" --no-deps
 cd $AREAL_PATH
 
 # Install AReaL
-pip install -e .
+uv pip install -e .
