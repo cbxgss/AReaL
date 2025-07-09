532a26f14cd1388a82e99312293dfdc43a667a78
chengrong <chengrong@tju.edu.cn>
2025-07-09 16:01:21 +0800

    fix

--- run.sh
+++ run.sh
@@ -31,12 +31,12 @@ experiment_name="async_ppo_qwen2.5"
 
 current_dir=$(pwd)
 
-mkdir -p ${current_dir}/outputs/$experiment_name/checkpoints
+mkdir -p ${current_dir}/outputs/$experiment_name
 
 python training/main_async_ppo.py \
     n_nodes=1 n_gpus_per_node=${gpu_nums} \
     allocation_mode=sglang.d4p1m1+d2p2m1 \
-    cluster.fileroot=${current_dir}/outputs/$experiment_name/checkpoints \
+    cluster.fileroot=${current_dir}/outputs/$experiment_name \
     actor.type._class=qwen2 \
     actor.path=Qwen/Qwen2.5-3B-Instruct \
     ref.type._class=qwen2.5 \
@@ -44,7 +44,7 @@ python training/main_async_ppo.py \
     dataset.path=$HF_HOME/hub/datasets--inclusionAI--AReaL-RL-Data/snapshots/07dfc8977909d38366d4e913eb3648a939e5aeb4/data/deepscaler_40k_0319.jsonl \
     dataset.train_bs_n_seqs=32 \
     group_size=8 \
-    ppo.gen.max_new_tokens=4096 \
+    ppo.gen.max_new_tokens=10240 \
     ppo.ppo_n_minibatches=4 \
     actor.sglang.attention_backend=fa3 \
     actor_train.mb_spec.max_tokens_per_mb=32768 \
